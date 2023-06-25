f6510d5f12f81e5bddb25f2a11620c80ebea12a0
Gonzanapo <gonzanapo@gmail.com>
2023-06-25 00:46:02 -0300

    components move to src instead pages

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
 import { api } from "~/utils/api";
 
 export default function Register() {
