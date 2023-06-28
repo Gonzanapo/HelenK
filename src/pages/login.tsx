2589aa22feb778df9665362281d0bc05977d25d3
Gonzanapo <gonzanapo@gmail.com>
2023-06-28 20:01:37 -0300

    _

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -25,11 +25,7 @@ export default function Register() {
       <main className="mainForm">
         <FormLogin />
       </main>
-    <div className="separador">
-      <div className="separator">
-        <span className="linea-o bg-white p-2">o</span>
-      </div>
-    </div>
+
      
       <main className="bottom-main-register">
         <div>
