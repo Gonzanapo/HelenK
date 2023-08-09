0e7873e695cdb34fb9d539360825e634e6073f53
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2023-08-09 15:48:49 -0300

    fix some build errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,13 +8,18 @@ import "../styles/register.css";
 import "../styles/login.css";
 import "../styles/resetpwd.css";
 import Head from "next/head";
-import { MantineProvider } from '@mantine/core';
-import { Notifications } from '@mantine/notifications';
+import { MantineProvider, ColorSchemeProvider, type ColorScheme } from "@mantine/core";
+import { useState } from "react";
+import { Notifications } from "@mantine/notifications";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   return (
     <>
       <Head>
@@ -22,12 +27,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
         {/* <link rel="icon" href="/Logo.ico" /> */}
         <meta name="description" content="HelenK" />
       </Head>
-      <SessionProvider session={session}>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
         <MantineProvider withNormalizeCSS withGlobalStyles>
-          <Notifications />
-          <Component {...pageProps} />
+          <SessionProvider session={session}>
+            <Notifications />
+            <Component {...pageProps} />
+          </SessionProvider>
         </MantineProvider>
-      </SessionProvider>
+      </ColorSchemeProvider>
     </>
   );
 };
