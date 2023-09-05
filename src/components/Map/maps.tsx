42de81703590d794c26e953b1830eaa83acc2d72
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2023-09-05 10:40:16 -0300

    style map

--- src/components/Map/maps.tsx
+++ src/components/Map/maps.tsx
@@ -1,85 +1,97 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, { useEffect, useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 // Establece el ancho y el alto del componente al 100%
 const containerStyle = {
-    width: '100%',
-    height: '100%'
+  width: "100%",
+  height: "100%",
 };
 
 const center = {
-    lat: -34.564959,
-    lng: -58.450324,
+  lat: -34.564959,
+  lng: -58.450324,
 };
 
 // Define las opciones de mapa
 const mapOptions = {
-    center: center,
-    fullscreenControl: true,
-    mapTypeControl: false,
-    streetViewControl: false,
-    zoom: 16,
-    zoomControl: true,
-    maxZoom: 20000,
-    mapId: "a1167de20e6b3769"
+  center: center,
+  fullscreenControl: true,
+  mapTypeControl: false,
+  streetViewControl: false,
+  zoom: 16,
+  zoomControl: true,
+  maxZoom: 20000,
+  mapId: "a1167de20e6b3769",
 };
 
 export default function Maps() {
-    const [position, setPosition] = useState({ lat: 0, lng: 0 });
-    const [realTime, setRealTime] = useState(null);
-    const [circulo, setCirculo] = useState(null);
-    const [markers, setMarkers] = useState([]);
-    const [circulos, setCirculos] = useState([]);
-    const [coordsUbi, setCoordsUbi] = useState([0, 0]);
-    const [semaforos, setSemaforos] = useState(null);
-    const [minDistance, setMinDistance] = useState(50000);
+  const [position, setPosition] = useState({ lat: 0, lng: 0 });
+  const [realTime, setRealTime] = useState(null);
+  const [circulo, setCirculo] = useState(null);
+  const [markers, setMarkers] = useState([]);
+  const [circulos, setCirculos] = useState([]);
+  const [coordsUbi, setCoordsUbi] = useState([0, 0]);
+  const [semaforos, setSemaforos] = useState(null);
+  const [minDistance, setMinDistance] = useState(50000);
 
-    // Crea refs para cada elemento que quieras seleccionar
-    const mapViewRef = useRef(null);
-    const initialStatePanelRef = useRef(null);
-    const modalRef = useRef(null);
+  // Crea refs para cada elemento que quieras seleccionar
+  const mapViewRef = useRef(null);
+  const initialStatePanelRef = useRef(null);
+  const modalRef = useRef(null);
 
-    // Utiliza las refs para acceder a los elementos del DOM
-    const commutesEl = {
-        map: mapViewRef.current,
-        initialStatePanel: initialStatePanelRef.current,
-        modal: modalRef.current,
-    };
+  // Utiliza las refs para acceder a los elementos del DOM
+  const commutesEl = {
+    map: mapViewRef.current,
+    initialStatePanel: initialStatePanelRef.current,
+    modal: modalRef.current,
+  };
 
-    // Accede a las propiedades del objeto commutesEl
-    const mapElement = commutesEl.map;
-    const initialStatePanelElement = commutesEl.initialStatePanel;
-    const modalElement = commutesEl.modal;
+  // Accede a las propiedades del objeto commutesEl
+  const mapElement = commutesEl.map;
+  const initialStatePanelElement = commutesEl.initialStatePanel;
+  const modalElement = commutesEl.modal;
 
-    useEffect(() => {
-        // Rest of the code remains the same as before ...
-    }, []);
+  useEffect(() => {
+    // Rest of the code remains the same as before ...
+  }, []);
 
-    // The rest of the code remains the same as before ...
+  // The rest of the code remains the same as before ...
 
-    return (
-        <div className='Map'>
-            <div className='buttons-routes'>
-
-            <button className="button-commute"> Seguimiento de Ruta </button>
-            <button className="button-route"> Ruta </button>
+  return (
+    <div className="Map">
+      <div className="buttons-routes">
+        <h2 className="button-commute"> Seguimiento de Ruta </h2>
+        <button className="button-route">
+          <li className="h2-route">
+            {" "}
+            En camino <br /> <h4 className="h4-route">
+              {" "}
+              Salida 12:22:45 AM{" "}
+            </h4>{" "}
+          </li>
+          <li className="h2-route">
+            {" "}
+            10. min restantes <br />{" "}
+            <h4 className="h4-route"> Llegada 12:55:14 AM </h4>{" "}
+          </li>
+        </button>
+      </div>
+      <div className="commutes">
+        <div className="commutes-map" aria-label="Map" id="Map">
+          <LoadScript googleMapsApiKey="AIzaSyBEJtEhY1iMBrrsDlLMUxbzk-bvZrpJHBQ&">
+            <div className="map-view" ref={mapViewRef}>
+              {/* Pasa el objeto mapOptions como prop al componente GoogleMap */}
+              <GoogleMap
+                id="GoogleMap"
+                mapContainerStyle={containerStyle}
+                options={mapOptions}
+              ></GoogleMap>
             </div>
-            <div className="commutes">
-                <div className="commutes-map" aria-label="Map" id="Map">
-
-                    <LoadScript googleMapsApiKey="AIzaSyBEJtEhY1iMBrrsDlLMUxbzk-bvZrpJHBQ&">
-                        <div className='map-view' ref={mapViewRef}>
-                            {/* Pasa el objeto mapOptions como prop al componente GoogleMap */}
-                            <GoogleMap id='GoogleMap' mapContainerStyle={containerStyle} options={mapOptions}></GoogleMap>
-                        </div>
-                    </LoadScript>
-
-                </div>
-
-            </div>
-
+          </LoadScript>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
