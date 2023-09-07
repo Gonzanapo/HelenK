7c28f4bf401d31aff1d2ef7f05718e39c97abc6f
Gonzanapo <gonzanapo@gmail.com>
2023-09-07 19:48:10 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,12 +1,24 @@
-const withPWA = require('next-pwa')({
-  dest: 'public'
+const withPWA = require("next-pwa")({
+  dest: "public",
   // disable: process.env.NODE_ENV === 'development',
   // register: true,
   // scope: '/app',
   // sw: 'service-worker.js',
   //...
-})
-
+});
 module.exports = withPWA({
-  // next.js config
-})
+  //   // next.js config
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  // next-pwa config
+  pwa: {
+    dest: "public",
+    register: true,
+    scope: "/",
+    sw: "service-worker.js",
+    skipWaiting: true,
+  },
+});
