6e1fa0d10159e12796bd2b1e43592bb1f34ee02f
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2023-09-12 13:19:25 -0300

    slide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,25 @@
-
-
 import React, { useEffect, useState } from "react";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import SessionChecker from "~/components/Sign/sessionChecker";
 import Maps from "~/components/Map/maps";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import { useSession } from "next-auth/react";
 import { NotUser } from "~/components/image";
-import Link from "next/link"; // Import Link component from next/link
-
+import Link from "next/link";
+import { useSwipeable } from 'react-swipeable';
 export default function Mapa() {
-  useEffect(() => {
-    // Rest of the code remains the same as before
-    // ...
-  }, []);
-
-  // The rest of the code remains the same as before
-  // ...
-
   const { data: session } = useSession();
-
-  // Define state variable and setter function for navbar visibility
   const [showNavbar, setShowNavbar] = useState(false);
-
-  // Define a function to toggle the navbar visibility
   const handleFooterClick = () => {
     setShowNavbar(!showNavbar);
   };
-
+  const handleSwipeUp = () => { setShowNavbar(true); };
+  const handleSwipeDown = () => { setShowNavbar(false); };
+  const handlers = useSwipeable({ onSwipedUp: handleSwipeUp, onSwipedDown: handleSwipeDown });
   return (
     <div className="Contenedor-map">
       <SessionChecker />
-      <header className="top-header-map">
-        {/* Placeholder for the logo */}
-        <a href="#">
-          <div className="marca-map">
-            {/* <img src="LOGO.svg" alt="Logo" /> */}
-          </div>
-        </a>
-      </header>
-      <header className="header-map">
-        {/* Placeholder for navigation links */}
-        {/* ... */}
-      </header>
       <main className="main-map">
         <Maps />
       </main>
-      {/* Añade una condición al atributo className del footer */}
-
-      {/* Add a conditional rendering expression for the navbar */}
       {showNavbar && (
         <nav className={`navbar-map ${showNavbar ? "clicked" : ""}`} onClick={handleFooterClick}>
           <Link href="/">
@@ -63,10 +33,9 @@ export default function Mapa() {
       <footer
         className={`footer-map ${showNavbar ? "clicked" : ""}`}
         onClick={handleFooterClick}
+        {...handlers}
       >
-
         {session ? (
-          // Usar un fragmento para envolver los elementos
           <>
             <img
               className={`img-user ${showNavbar ? "clicked" : ""}`}
@@ -77,7 +46,7 @@ export default function Mapa() {
             <h1 className={`username ${showNavbar ? "clicked" : ""}`}> {session.user.name}</h1>
           </>
         ) : (
-          <>
+          < >
             <NotUser />
             <h1 className={`username ${showNavbar ? "clicked" : ""}`}>
               Invitado
@@ -85,14 +54,6 @@ export default function Mapa() {
           </>
         )}
       </footer>
-
-
     </div>
   );
 }
-
-// <footer className="footer-responsive">
-// HOLIS
-// {/* Placeholder for responsive footer content */}
-// {/* ... */}
-// </footer>
